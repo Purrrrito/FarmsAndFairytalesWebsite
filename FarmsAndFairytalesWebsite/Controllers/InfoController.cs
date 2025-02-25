764aa23059a750b47ccc0b252b06588424c88729
Wyatt Smith <wyattjorgen@gmail.com>
2025-02-24 17:47:33 -0800

    Added empty views for the next pages that need  to be created. Added all needed buttons to the nav-bar. Styled the nav-bar.

---
+++ FarmsAndFairytalesWebsite/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FarmsAndFairytalesWebsite.Controllers
+{
+	public class InfoController : Controller
+	{
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult FAQ()
+		{
+			return View();
+		}
+	}
+}
