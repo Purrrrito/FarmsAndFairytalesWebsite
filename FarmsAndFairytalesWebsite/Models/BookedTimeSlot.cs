66417c9ec0297719d9f07307ee1cf90b0a812ccd
Wyatt Smith <wyattjorgen@gmail.com>
2025-03-10 17:38:10 -0700

    New and updated models, and deleted the old ones

---
+++ FarmsAndFairytalesWebsite/Models/BookedTimeSlot.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmsAndFairytalesWebsite.Models
+{
+	public enum BookingType
+	{
+		General,
+		Boudoir,
+		Milestone
+	}
+
+	public class BookedTimeSlots
+	{
+		[Key]
+		public int BookedTimeSlotId { get; set; }
+
+		[Required]
+		public DateTime StartTime { get; set; }
+
+		[Required]
+		public DateTime EndTime { get; set; }
+
+		[Required]
+		public bool IsOutdoor { get; set; } // True for outdoor, false for indoor
+
+		public BookingType Type { get; set; } // General, Boudoir, Milestone
+
+		public IdentityUser? Photographer { get; set; }
+	}
+}
--- FarmsAndFairytalesWebsite/Models/Events.cs
+++ FarmsAndFairytalesWebsite/Models/Events.cs
@@ -3,32 +3,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FarmsAndFairytalesWebsite.Models
 {
-    public class Event
-    {
-        [Key]
-        public int EventId { get; set; }
+	public class Event
+	{
+		[Key]
+		public int EventId { get; set; }
 
-        [Display(Name = "Date of the Event")]
-        public DateTime DateOfEvent { get; set; }
+		[Display(Name = "Date of the Event")]
+		public DateTime DateOfEvent { get; set; }
 
-        [Display(Name = "Event's name")]
-        public string EventName { get; set; }
+		[Display(Name = "Event's name")]
+		public string EventName { get; set; }
 
-        [StringLength(300, ErrorMessage = "The description cannot be longer than 300 characters.")]
-        [Display(Name = "Event description")]
-        public string Description { get; set; }
+		[StringLength(300, ErrorMessage = "The description cannot be longer than 300 characters.")]
+		[Display(Name = "Event description")]
+		public string Description { get; set; }
 
-        [Display(Name = "Photographer's name")]
-        public string PhotographerHost { get; set; }
+		[Display(Name = "Photographer's name")]
+		public string PhotographerHost { get; set; }
 
-        [Display(Name = "Contact information")]
-        public string ContactInfo { get; set; }
-
-        public IndoorBookedTimeSlots? IndoorEventTimeSlots { get; set; }
-
-		public OutdoorBookedTimeSlots? OutdoorEventTimeSlots { get; set; }
+		[Display(Name = "Contact information")]
+		public string ContactInfo { get; set; }
 
 		[Display(Name = "Event created by")]
 		public IdentityUser? Photographer { get; set; }
+
+		public BookedTimeSlots? EventTimeSlot { get; set; } // Now references the new unified model
 	}
 }
